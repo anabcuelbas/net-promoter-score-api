5283c44b5263e6670bcbcc889c9660028ac1b97c
anabcuelbas <abcuelbas@gmail.com>
2021-02-23 15:53:21 -0300

    Cria modelo de usuário e função de criação

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,30 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
     async create(request: Request, response: Response) {
-        const body = request.body;
-        console.log(body);
+        const { name, email } = request.body;
+        
+        const usersRepository = getRepository(User);
 
-        return response.send();
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists) {
+            return response.status(400).json({
+                error: "User already exists!",
+            })
+        }
+
+        const user = usersRepository.create({
+            name, email
+        });
+
+        await usersRepository.save(user);
+
+        return response.json(user);
     }
 }
 
