dce4d7b99418f8cb60496b971082177edb07a349
anabcuelbas <abcuelbas@gmail.com>
2021-02-27 16:29:12 -0300

    Cria arquivos e funções relacionados a Surveys

---
+++ src/database/migrations/1614452317828-CreateSurveys.ts
@@ -0,0 +1,36 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSurveys1614452317828 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "surveys",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "title",
+                        type: "varchar"
+                    },
+                    {
+                        name: "description",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
