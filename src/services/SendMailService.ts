63469e540e393c80936b04cee9414a5a17d8ac45
anabcuelbas <abcuelbas@gmail.com>
2021-02-28 15:52:11 -0300

    Cria estilização para o email

--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,5 +1,8 @@
 import nodemailer, { Transporter } from 'nodemailer';
-
+import { resolve } from 'path';
+import handlebars from 'handlebars';
+import fs from 'fs';
+ 
 class SendMailService {
     private client: Transporter;
 
@@ -20,11 +23,21 @@ class SendMailService {
     }
 
     async execute(to: string, subject: string, body: string) {
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+        const templateFileContent = fs.readFileSync(npsPath).toString("utf8");
+
+        const mailTemplateParse = handlebars.compile(templateFileContent);
+
+        const html = mailTemplateParse({
+            name: to,
+            title: subject,
+            description: body
+        })
 
         const message = await this.client.sendMail({
             to,
             subject,
-            html: body,
+            html,
             from: "NPS <noreplay@nps.com.br>"
         })
 
