f0b8f5519016d83059026741d8e739ac511c9304
anabcuelbas <abcuelbas@gmail.com>
2021-02-28 20:16:20 -0300

    Faz calculo de NPS

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
@@ -10,6 +11,7 @@ const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 
@@ -19,5 +21,7 @@ router.get("/surveys", surveysController.show);
 router.post("/sendMail", sendMailController.execute);
 
 router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
