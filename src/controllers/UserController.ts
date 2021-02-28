418afab5935248b3a497755dc4c4dbebbc06f28f
anabcuelbas <abcuelbas@gmail.com>
2021-02-28 20:29:03 -0300

    Adiciona validação no cadastro de usuário

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,26 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup';
 
 class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+        })
+
+        /* if(!(await schema.isValid(request.body))) {
+            return response.status(400).json({error: "Validation Failed!"});
+        } */
+
+        try {
+            await schema.validate(request.body, { abortEarly: false });
+        } catch (err) {
+            return response.status(400).json({ error: err });
+        }
         
         const usersRepository = getCustomRepository(UsersRepository);
 
