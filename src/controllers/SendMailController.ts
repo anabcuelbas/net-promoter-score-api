ba00061cad5d496c9a348039226d3a9f1285fa7b
anabcuelbas <abcuelbas@gmail.com>
2021-02-28 16:51:58 -0300

    Faz mostrar objeto completo na pesquisa

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -41,7 +41,8 @@ class SendMailController {
         const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
 
         const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-            where: [{user_id: user.id}, {value: null}]
+            where: [{user_id: user.id}, {value: null}],
+            relations: ["user", "survey"]
         })
 
         if(surveyUserAlreadyExists) {
